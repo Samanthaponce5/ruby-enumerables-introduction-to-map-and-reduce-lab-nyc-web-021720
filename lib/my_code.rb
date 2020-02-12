3443fad4d2a9f76c70b9434918c9ccf2f7b3642f
Samantha Ponce <samanthaponce5@gmail.com>
2020-02-12 03:43:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -71,11 +71,11 @@ end
 
 def reduce_to_any_true(source_array)
   i = 0 
-  total = 0 
+  
   while i < source_array.length 
-  source_array[i]
+ total =  source_array[i]
   i += 1 
   
 end
-  
+ total 
 end
