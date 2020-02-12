0bb71847c22369e4615e3e579e0734507bfb9b9c
Samantha Ponce <samanthaponce5@gmail.com>
2020-02-12 03:29:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,8 +49,6 @@ def reduce_to_total(source_array, starting_point = 0)
   
   while i < source_array.length
   if source_array !=starting_point
-    source_array[i]
-  else
     source_array[i]+ starting_point
   end
   i +=1
